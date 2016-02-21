3364b8c0c12fb2e85c6966d992bd05a3a9626ae1
JosuTC <jpalmerotc1994@gmail.com>
2016-02-21 19:36:19 +0000

    Función mostrar_prácticas

---
+++ js/json/dsi.js
@@ -0,0 +1,12 @@
+{
+    "Asignatura": "Desarrollo de Sistemas Informáticos",
+    "practicas": [
+        {
+            "id" : 1,
+            "titulo": "Tareas Iniciales",
+            "imagen": "imagenes_practicas/practica1.png",
+            "url": "http://github.com/JosueTC94/tareas-iniciales-josue-nayra-dsi15-16/tree/master",
+            "url_paginaweb": "http://JosueTC94.github.io/tareas-iniciales-josue-nayra-dsi15-16/tree/master"
+        }
+    ]
+}
