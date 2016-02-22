27001bcf75934ad11da376c90fc3419085906879
Josue Toledo Castro <alu0100763492@ull.edu.es>
2016-02-22 13:08:19 +0000

    Añadiendo target blank al enlace de la practica

--- js/json/dsi.js
+++ js/json/dsi.js
@@ -5,7 +5,7 @@
             "id" : 1,
             "titulo": "Tareas Iniciales",
             "imagen": "imagenes_practicas/practica1.png",
-            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/tareas-iniciales-josue-nayra-dsi15-16/blob/master/index.html",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/tareas-iniciales-josue-nayra-dsi15-16/",
             "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/tareas-iniciales-josue-nayra-dsi15-16/"
         }
     ]
