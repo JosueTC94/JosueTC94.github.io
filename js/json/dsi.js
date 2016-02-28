f619485466b752ed6d90c67a444dae347cc06b25
josuetc94 <alu0100763492@ull.edu.es>
2016-02-28 21:34:57 +0000

    Añadiendo práctica 2

--- js/json/dsi.js
+++ js/json/dsi.js
@@ -7,6 +7,13 @@
             "imagen": "imagenes_practicas/practica1.png",
             "url": "https://github.com/ULL-ESIT-GRADOII-DSI/tareas-iniciales-josue-nayra-dsi15-16/",
             "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/tareas-iniciales-josue-nayra-dsi15-16/"
+        },
+        {
+            "id" : 2, 
+            "titulo": "Conversor de temperatura",
+            "imagen": "imagenes_practicas/conversor.jpg",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/introduccion-josue-nayra-dsi15-16.git",
+            "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/introduccion-josue-nayra-dsi15-16/"
         }
     ]
 }
