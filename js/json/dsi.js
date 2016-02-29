54d44b890e2f222b78293c1d275ffb74f2b5381d
josuetc94 <alu0100763492@ull.edu.es>
2016-02-29 09:55:17 +0000

    Actualizando dsi.js dirección práctica 2 repositorio

--- js/json/dsi.js
+++ js/json/dsi.js
@@ -12,7 +12,7 @@
             "id" : 2, 
             "titulo": "Conversor de temperatura",
             "imagen": "imagenes_practicas/conversor.jpg",
-            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/introduccion-josue-nayra-dsi15-16.git",
+            "url": "https://github.com/ULL-ESIT-GRADOII-DSI/introduccion-josue-nayra-dsi15-16/tree/gh-pages",
             "url_paginaweb": "http://ull-esit-gradoii-dsi.github.io/introduccion-josue-nayra-dsi15-16/"
         }
     ]
